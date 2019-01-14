12e72582c9aa6ae7ca3788e4951a3272f64c34ff
Cupaiuoli <lautaro.cupaiuoli@gmail.com>
2019-01-13 22:02:53 -0300

    Configuracion del proyecto con webpack

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ReactJs - Tutorial FaztWeb - https://www.youtube.com/watch?v=ll4pjr_uH3I" 
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const Htmlwebpackplugin = require('html-webpack-plugin');
+module.exports = {
+   entry: './src/app/index.js' ,
+   output: {
+       path: path.join(__dirname, 'dist'),
+       filename: 'bundle.js'
+       
+       
+   },
+   devServer: {
+       port: 3000
+   },
+   module: {
+       rules: [
+           {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/
+           }
+       ]
+    },
+    plugins: [
+        new Htmlwebpackplugin({
+            template: './src/index.html'
+
+        })
+    ]      
+}
