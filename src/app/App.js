473b7ec4f574c3dd4667b5eff3b85e5b4be3ece8
Cupaiuoli <lautaro.cupaiuoli@gmail.com>
2019-01-17 18:55:34 -0300

    Tabla Bootstrap en un componente

--- src/app/App.js
+++ src/app/App.js
@@ -5,7 +5,35 @@ class App extends React.Component {
         console.log(this.props.data);
         console.log(this.props.title);
         console.log(this.props.headings);
-        return <h1>Bienvenido!</h1>
+        return (
+            <div className="container p-4">
+                <h1>{this.props.title}</h1>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            {
+                                this.props.headings.map((headings, i) => {
+                                    return <th key={i}>{headings}</th>
+                                })
+                            }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.data.map((row, i) => {
+                                return <tr key={i}>
+                                    <td>{row.when}</td>
+                                    <td>{row.who}</td>
+                                    <td>{row.description}</td>
+
+                                </tr>
+                    
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
 
     }
 }
